b3f3c4c3bd5cdd0bb33db87ce45a3e81011931b7
aconamos <nephi.norton@gmail.com>
2021-11-16 20:32:09 -0700

    minor t hing

--- src/main/java/net/aconamos/autodc/AutoDCScreen.java
+++ src/main/java/net/aconamos/autodc/AutoDCScreen.java
@@ -20,15 +20,16 @@ public class AutoDCScreen extends Screen {
     }
 
     public final void init() {
-        this.enableButton = new ButtonWidget(this.width / 2 - 100, this.height / 2, 98, 20, Text.of("AutoDC: " + Config.AutoDCEnabled), (button) -> {
+        this.enableButton = new ButtonWidget(this.width / 2 - 100, this.height / 2, 98, 20, Text.of("AutoDC: " + (Config.AutoDCEnabled ? "Enabled" : "Disabled")), (button) -> {
             Config.AutoDCEnabled = !Config.AutoDCEnabled;
-            button.setMessage(Text.of("AutoDC: " + Config.AutoDCEnabled));
+            button.setMessage(Text.of("AutoDC: " + (Config.AutoDCEnabled ? "Enabled" : "Disabled")));
         });
         this.addDrawableChild(this.enableButton);
         this.menuButton = new ButtonWidget(this.width / 2 - 100, this.height / 2 + 22, 200, 20, ScreenTexts.DONE, (button -> this.client.setScreen(this.parent)));
         this.addDrawableChild(this.menuButton);
         this.ticksBox = new TextFieldWidget(this.textRenderer, this.width / 2 + 3, this.height / 2 + 2, 95, 16, this.ticksBox, Text.of("Ticks required for DC"));
         this.ticksBox.setChangedListener((number) -> Config.ticksRequired = number);
+        this.ticksBox.setText(Config.ticksRequired);
         this.addSelectableChild(this.ticksBox);
         this.setInitialFocus(this.ticksBox);
     }
